5b7a645da13dd57071c85fe7fe8a1b942d067427
arthurDiff <arthur.lee945@gmail.com>
2025-01-15 22:47:21 -0600

    proc macro extern crate fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,6 @@ mod result_error;
 pub mod sync;
 
 pub use crate::result_error::*;
+
+#[cfg(any(feature = "proc_macro", feature = "comp"))]
+extern crate proc_macro_a;
--- src/proc_macro.rs
+++ src/proc_macro.rs
@@ -1,6 +1,3 @@
 #[cfg(any(feature = "proc_macro", feature = "comp"))]
-extern crate proc_macro_a;
-
-#[cfg(any(feature = "proc_macro", feature = "comp"))]
 #[cfg_attr(docsrs, doc(any(feature = "proc_macro", feature = "comp")))]
 pub use proc_macro_a::comp;
